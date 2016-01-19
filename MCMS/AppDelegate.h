f48b90ca952217ec73662150b97dfcaedf95d3ac
Ceasar Barbosa <CeazB@ceasars-macbook-pro.local>
2016-01-19 14:07:50 -0600

    Initial Commit

---
+++ MCMS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCMS
+//
+//  Created by Ceasar Barbosa on 1/19/16.
+//  Copyright © 2016 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
